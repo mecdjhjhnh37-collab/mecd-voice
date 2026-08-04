b431c422d8ce2eac02074c6ed6d47e4bb6197a17
mecdjhjhnh37-collab <mecdjhjhnh37@gmail.com>
2026-08-04 14:46:56 +0300

    Update translations for Arabic and Turkish

--- language.js
+++ language.js
@@ -1,21 +1,54 @@
 const translations = {
 
-ar:{
-title:"موقع المطور مجد",
-online:"🟢 متصل الآن",
-create:"🎙️ إنشاء غرفة",
-join:"🚪 دخول غرفة",
-rooms:"🌍 الغرف العامة",
-settings:"⚙️ الإعدادات"
+ar: {
+
+title: "🎙️ موقع المطور مجد",
+
+welcome:
+"مرحبًا بك في موقع المطور مجد",
+
+online:
+"🟢 متصل الآن",
+
+create:
+"🎙️ إنشاء غرفة",
+
+join:
+"🚪 دخول غرفة",
+
+rooms:
+"🌍 الغرف العامة",
+
+settings:
+"⚙️ الإعدادات"
+
 },
 
-tr:{
-title:"Mecd Geliştirici",
-online:"🟢 Şimdi çevrimiçi",
-create:"🎙️ Oda oluştur",
-join:"🚪 Odaya katıl",
-rooms:"🌍 Genel odalar",
-settings:"⚙️ Ayarlar"
+
+
+tr: {
+
+title:
+"🎙️ Mecd Geliştirici",
+
+welcome:
+"Mecd Geliştirici'nin sitesine hoş geldiniz",
+
+online:
+"🟢 Şimdi çevrimiçi",
+
+create:
+"🎙️ Oda oluştur",
+
+join:
+"🚪 Odaya katıl",
+
+rooms:
+"🌍 Genel odalar",
+
+settings:
+"⚙️ Ayarlar"
+
 }
 
 };
