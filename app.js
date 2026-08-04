372d87685ec4475be986d48fb4c41bdcf0fb2ed0
mecdjhjhnh37-collab <mecdjhjhnh37@gmail.com>
2026-08-04 10:30:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,3 @@
-import { auth, provider } from "./firebase.js";
-import { signInWithPopup } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-// زر تغيير اللغة
-let arabic = true;
-
-document.getElementById("langToggleBtn").onclick = function(){
-
-  if(arabic){
-    document.getElementById("title").innerHTML = "🎙️ Mecd Geliştirici";
-    document.getElementById("welcome").innerHTML =
-    "Mecd Geliştirici'nin sitesine hoş geldiniz";
-  }else{
-    document.getElementById("title").innerHTML = "موقع المطور مجد";
-    document.getElementById("welcome").innerHTML =
-    "مرحبًا بك في موقع المطور مجد";
-  }
-
-  arabic = !arabic;
-};
-
-
-// زر تسجيل الدخول Google
 document.getElementById("googleLogin").onclick = async function(){
 
   try{
@@ -29,12 +6,12 @@ document.getElementById("googleLogin").onclick = async function(){
 
     const user = result.user;
 
-    alert("مرحبًا " + user.displayName);
+    alert("تم تسجيل الدخول: " + user.displayName);
 
-  }catch(error){
+  } catch(error){
 
+    alert(error.message);
     console.log(error);
-    alert("حدث خطأ في تسجيل الدخول");
 
   }
 
