9f9ce9ce46fc250914d070968363a3a2e5a9680a
mecdjhjhnh37-collab <mecdjhjhnh37@gmail.com>
2026-08-04 11:40:20 +0300

    Create language.js

---
+++ language.js
@@ -0,0 +1,21 @@
+const translations = {
+
+ar:{
+title:"موقع المطور مجد",
+online:"🟢 متصل الآن",
+create:"🎙️ إنشاء غرفة",
+join:"🚪 دخول غرفة",
+rooms:"🌍 الغرف العامة",
+settings:"⚙️ الإعدادات"
+},
+
+tr:{
+title:"Mecd Geliştirici",
+online:"🟢 Şimdi çevrimiçi",
+create:"🎙️ Oda oluştur",
+join:"🚪 Odaya katıl",
+rooms:"🌍 Genel odalar",
+settings:"⚙️ Ayarlar"
+}
+
+};
