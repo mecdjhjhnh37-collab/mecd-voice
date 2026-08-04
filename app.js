41d6d91cff42c4ad3683ecf94afeb4712859bb8c
mecdjhjhnh37-collab <mecdjhjhnh37@gmail.com>
2026-08-04 09:26:15 +0300

    Refactor changeLang function to window scope.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-function changeLang(){
+window.changeLang = function () {
   document.getElementById("title").innerHTML = "🎙️ Mecd Geliştirici";
-  document.getElementById("welcome").innerHTML = "Mecd Geliştirici'nin sitesine hoş geldiniz";
-}
+  document.getElementById("welcome").innerHTML =
+    "Mecd Geliştirici'nin sitesine hoş geldiniz";
+};
 
-document.getElementById("googleLogin").onclick = function(){
+document.getElementById("googleLogin").onclick = function () {
   alert("زر تسجيل الدخول يعمل");
 };
