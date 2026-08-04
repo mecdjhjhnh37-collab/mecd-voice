c3d740826fe2364a6d85518ef3e5d897170dd953
mecdjhjhnh37-collab <mecdjhjhnh37@gmail.com>
2026-08-04 10:37:17 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -14,4 +14,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
 export const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account"
+});
