3f79df59d7f0b5a498080d71c470f40c5cb604a7
mecdjhjhnh37-collab <mecdjhjhnh37@gmail.com>
2026-08-04 11:08:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,41 @@
 let arabic = true;
 
 
+// زر تغيير اللغة
 document.getElementById("langToggleBtn").onclick = function(){
 
-if(arabic){
+  if(arabic){
 
-document.getElementById("title").textContent =
-"🎙️ Mecd Geliştirici";
+    document.getElementById("title").textContent =
+    "🎙️ Mecd Geliştirici";
 
-document.getElementById("welcome").textContent =
-"Mecd Geliştirici'nin sitesine hoş geldiniz";
+    document.getElementById("welcome").textContent =
+    "Mecd Geliştirici'nin sitesine hoş geldiniz";
 
-document.getElementById("continueBtn").textContent =
-"➡️ Devam et";
+    document.getElementById("continueBtn").textContent =
+    "➡️ Devam et";
 
-}else{
+  }else{
 
-document.getElementById("title").textContent =
-"🎙️ موقع المطور مجد";
+    document.getElementById("title").textContent =
+    "🎙️ موقع المطور مجد";
 
-document.getElementById("welcome").textContent =
-"مرحبًا بك في موقع المطور مجد";
+    document.getElementById("welcome").textContent =
+    "مرحبًا بك في موقع المطور مجد";
 
-document.getElementById("continueBtn").textContent =
-"➡️ أكمل";
+    document.getElementById("continueBtn").textContent =
+    "➡️ أكمل";
 
-}
+  }
 
-arabic = !arabic;
+  arabic = !arabic;
 
 };
 
 
-
+// زر أكمل
 document.getElementById("continueBtn").onclick = function(){
 
-alert("أهلاً بك في الموقع 🚀");
+  window.location.href = "home.html";
 
 };
