b56d55d10b78993d8e4175593644fb79f5a8641c
mecdjhjhnh37-collab <mecdjhjhnh37@gmail.com>
2026-08-04 10:47:38 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,53 +1,5 @@
-import { auth, provider } from "./firebase.js";
-import { signInWithPopup } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+document.getElementById("langToggleBtn").onclick = function(){
 
-
-// زر تغيير اللغة
-const langButton = document.getElementById("langToggleBtn");
-
-let arabic = true;
-
-langButton.onclick = () => {
-
-  if(arabic){
-
-    document.getElementById("title").textContent =
-    "🎙️ Mecd Geliştirici";
-
-    document.getElementById("welcome").textContent =
-    "Mecd Geliştirici'nin sitesine hoş geldiniz";
-
-  }else{
-
-    document.getElementById("title").textContent =
-    "🎙️ موقع المطور مجد";
-
-    document.getElementById("welcome").textContent =
-    "مرحبًا بك في موقع المطور مجد";
-
-  }
-
-  arabic = !arabic;
-
-};
-
-
-// زر تسجيل الدخول
-const googleButton = document.getElementById("googleLogin");
-
-googleButton.onclick = async () => {
-
-  try {
-
-    const result = await signInWithPopup(auth, provider);
-
-    alert("مرحبًا " + result.user.displayName);
-
-  } catch(error){
-
-    console.log(error);
-    alert(error.message);
-
-  }
+alert("زر اللغة يعمل");
 
 };
